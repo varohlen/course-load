03776b126591f1101990a5d2a2e8f60423cca6f6
Viktor Arohlén <varohlen@gmail.com>
2024-08-14 13:16:36 +0200

    Quartz sync: Aug 14, 2024, 1:16 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "sv-SE",
+    locale: "en-EN",
     baseUrl: "summor.se",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
